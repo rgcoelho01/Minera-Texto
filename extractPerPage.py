3b9821c482d2b3c5905ab25d94d1a13e69bb2edb
lucasrobli <66880963+lucas-robli@users.noreply.github.com>
2021-06-20 15:13:29 -0300

    Update extractPerPage.py

--- extractPerPage.py
+++ extractPerPage.py
@@ -1,37 +1,52 @@
-import pdfplumber #nova biblioteca
-import pontuacoes #nesse arquivo contém um replace para remover as pontuacoes
-with pdfplumber.open("C:/Users/User/Documents/Tcc/projetoTccPatente3.pdf") as pdf: #estou utilizando essa biblioteca para extrair o texto dos pdfs
+
+import pdfplumber
+import pontuacoes
+with pdfplumber.open("C:/Users/User/Documents/Tcc/projetoTccPatente2.pdf") as pdf: #estou utilizando essa biblioteca para extrair o texto dos pdfs
     totalPages = len(pdf.pages)
 
-    i = 0;
+    i = 0
     reivindicacoes = []
     resumo = []
-    paginaReivindicacao = 0;
-    paginaResumo = 0;
+    paginaReivindicacao = 0
+    pageReivi = 0
+    pageResumo = 0
+    paginaResumo = 0
     while i < totalPages:
         first_page = pdf.pages[i]
-        conteudo = first_page.extract_text()
+        conteudo = first_page.extract_text().lower()
         texto = [[conteudo]]
+        lista = []
         for token in texto:
             for words in token:
                 word = pontuacoes.filtro(words)
                 conteudo = [[word]]
                 for textoFiltrado in conteudo:
+                    # print(textoFiltrado)
                     for textoSplitado in textoFiltrado:
                         alo = textoSplitado.split(" ")
-                        print(range(len(alo)))
+                        # print(alo)
                         for busca in alo:
-                            print(busca)
-                            if (busca == "REIVINDICAÇÕES" or busca == "REIVINDICAÇÃO"):
-                                reivindicacoes.append(alo)
-                                paginaReivindicacao = i
-                            if (busca == "RESUMO" or busca == 'Resumo'):
-                                print('encontrei')
-                                resumo.append(alo)
-                                paginaResumo = i
-        print('----------------------acabou--------------------------')
-        i+=1
-        print('Reivindicação: ',reivindicacoes)
-        print('Resumo: ', resumo)
-        print('pagina reivindicação', paginaReivindicacao)
-        print('pagina resumo', paginaResumo)
+                            if(busca != ''):
+                                lista.append(busca)
+        print(lista, 'alo')
+        for alo in lista:
+            if(lista[0] == "reivindicações" or lista[0] == "reivindicação"):
+                reivindicacoes.append(alo)
+                pageReivi = i
+
+            if(lista[0] == "resumo"):
+                resumo.append(alo)
+                pageResumo = i
+
+            if(pageReivi != 0):
+                if(lista[0] != "resumo"):
+                    reivindicacoes.append(alo)
+
+            if(pageResumo != 0):
+                if(pageResumo < i):
+                    resumo.append(alo)
+        i+= 1
+        
+    print('pageReivi', pageReivi, 'page resumo', pageResumo)
+    print('Reivindicação: ',reivindicacoes)
+    print('Resumo: ', resumo)
